25b9d00528653331a728252d18bf145606f47020
Roberto <robertophi@gmail.com>
2017-05-13 16:42:32 -0300

    Signed-off-by: Roberto <robertophi@gmail.com>

---
+++ Testes opencv/ROI/mainwindow.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(421, 472)
+
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+
+
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(421, 472))
+        MainWindow.setMaximumSize(QtCore.QSize(421, 472))
+        MainWindow.setDocumentMode(False)
+
+
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+
+
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(90, 60, 231, 191))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout.setSpacing(6)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.horizontalSlider_1 = QtWidgets.QSlider(self.verticalLayoutWidget)
+        self.horizontalSlider_1.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_1.setObjectName("horizontalSlider_1")
+        self.horizontalSlider_1.setRange(0,255)
+        self.verticalLayout.addWidget(self.horizontalSlider_1)
+
+        self.horizontalSlider_2 = QtWidgets.QSlider(self.verticalLayoutWidget)
+        self.horizontalSlider_2.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_2.setObjectName("horizontalSlider_2")
+        self.horizontalSlider_2.setRange(0,255)
+        self.verticalLayout.addWidget(self.horizontalSlider_2)
+
+        self.horizontalSlider_3 = QtWidgets.QSlider(self.verticalLayoutWidget)
+        self.horizontalSlider_3.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_3.setObjectName("horizontalSlider_3")
+        self.horizontalSlider_3.setRange(0,255)
+        self.verticalLayout.addWidget(self.horizontalSlider_3)
+
+        self.horizontalSlider_4 = QtWidgets.QSlider(self.verticalLayoutWidget)
+        self.horizontalSlider_4.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_4.setObjectName("horizontalSlider_4")
+        self.horizontalSlider_4.setRange(0,255)
+        self.verticalLayout.addWidget(self.horizontalSlider_4)
+
+        self.horizontalSlider_5 = QtWidgets.QSlider(self.verticalLayoutWidget)
+        self.horizontalSlider_5.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider_5.setObjectName("horizontalSlider_5")
+        self.horizontalSlider_5.setRange(0,255)
+        self.verticalLayout.addWidget(self.horizontalSlider_5)
+
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(20, 60, 71, 191))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout_2.setSpacing(6)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+
+
+        self.label_1 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_1.setObjectName("label_1")
+        self.verticalLayout_2.addWidget(self.label_1)
+
+        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+
+        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_2.addWidget(self.label_3)
+
+        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout_2.addWidget(self.label_4)
+
+        self.label_5 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_2.addWidget(self.label_5)
+
+
+
+
+
+        self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(320, 60, 71, 191))
+        self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_3.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout_3.setSpacing(6)
+
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.lcdNumber_1 = QtWidgets.QLCDNumber(self.verticalLayoutWidget_3)
+        self.lcdNumber_1.setObjectName("lcdNumber_1")
+        self.verticalLayout_3.addWidget(self.lcdNumber_1)
+
+        self.lcdNumber_2 = QtWidgets.QLCDNumber(self.verticalLayoutWidget_3)
+        self.lcdNumber_2.setObjectName("lcdNumber_2")
+        self.verticalLayout_3.addWidget(self.lcdNumber_2)
+
+        self.lcdNumber_3 = QtWidgets.QLCDNumber(self.verticalLayoutWidget_3)
+        self.lcdNumber_3.setObjectName("lcdNumber_3")
+        self.verticalLayout_3.addWidget(self.lcdNumber_3)
+
+        self.lcdNumber_4 = QtWidgets.QLCDNumber(self.verticalLayoutWidget_3)
+        self.lcdNumber_4.setObjectName("lcdNumber_4")
+        self.verticalLayout_3.addWidget(self.lcdNumber_4)
+
+        self.lcdNumber_5 = QtWidgets.QLCDNumber(self.verticalLayoutWidget_3)
+        self.lcdNumber_5.setObjectName("lcdNumber_5")
+        self.verticalLayout_3.addWidget(self.lcdNumber_5)
+
+
+
+
+        self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(80, 270, 111, 111))
+        self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_4)
+        self.verticalLayout_4.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout_4.setSpacing(6)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+
+        self.checkBox_1 = QtWidgets.QCheckBox(self.verticalLayoutWidget_4)
+        self.checkBox_1.setObjectName("checkBox_1")
+        self.verticalLayout_4.addWidget(self.checkBox_1)
+
+        self.checkBox_2 = QtWidgets.QCheckBox(self.verticalLayoutWidget_4)
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.verticalLayout_4.addWidget(self.checkBox_2)
+
+        self.checkBox_3 = QtWidgets.QCheckBox(self.verticalLayoutWidget_4)
+        self.checkBox_3.setObjectName("checkBox_3")
+        self.verticalLayout_4.addWidget(self.checkBox_3)
+
+        self.checkBox_4 = QtWidgets.QCheckBox(self.verticalLayoutWidget_4)
+        self.checkBox_4.setObjectName("checkBox_4")
+        self.verticalLayout_4.addWidget(self.checkBox_4)
+
+
+
+        self.verticalLayoutWidget_5 = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget_5.setGeometry(QtCore.QRect(230, 270, 111, 111))
+        self.verticalLayoutWidget_5.setObjectName("verticalLayoutWidget_5")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_5)
+        self.verticalLayout_5.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout_5.setSpacing(6)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+
+        self.checkBox_5 = QtWidgets.QCheckBox(self.verticalLayoutWidget_5)
+        self.checkBox_5.setObjectName("checkBox_5")
+        self.verticalLayout_5.addWidget(self.checkBox_5)
+
+        self.checkBox_6 = QtWidgets.QCheckBox(self.verticalLayoutWidget_5)
+        self.checkBox_6.setObjectName("checkBox_6")
+        self.verticalLayout_5.addWidget(self.checkBox_6)
+
+        self.checkBox_7 = QtWidgets.QCheckBox(self.verticalLayoutWidget_5)
+        self.checkBox_7.setObjectName("checkBox_7")
+        self.verticalLayout_5.addWidget(self.checkBox_7)
+
+        self.checkBox_8 = QtWidgets.QCheckBox(self.verticalLayoutWidget_5)
+        self.checkBox_8.setObjectName("checkBox_8")
+        self.verticalLayout_5.addWidget(self.checkBox_8)
+
+        self.pushButton_1 = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_1.setGeometry(QtCore.QRect(90, 390, 75, 23))
+        self.pushButton_1.setObjectName("pushButton_1")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(240, 390, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+
+
+        self.label_tittle = QtWidgets.QLabel(self.centralWidget)
+        self.label_tittle.setGeometry(QtCore.QRect(50, 10, 321, 31))
+        self.label_tittle.setObjectName("label_tittle")
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 421, 21))
+        self.menuBar.setObjectName("menuBar")
+        self.menuMain = QtWidgets.QMenu(self.menuBar)
+        self.menuMain.setObjectName("menuMain")
+        self.menuHelp = QtWidgets.QMenu(self.menuBar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName("mainToolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.actionHelp = QtWidgets.QAction(MainWindow)
+        self.actionHelp.setObjectName("actionHelp")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.menuMain.addAction(self.actionExit)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menuBar.addAction(self.menuMain.menuAction())
+        self.menuBar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_1.setText(_translate("MainWindow", "Upper"))
+        self.label_2.setText(_translate("MainWindow", "Lower"))
+        self.label_3.setText(_translate("MainWindow", "Thresh"))
+        self.label_4.setText(_translate("MainWindow", "Min Line Size"))
+        self.label_5.setText(_translate("MainWindow", "Label5"))
+        self.checkBox_1.setText(_translate("MainWindow", "Show frame"))
+        self.checkBox_2.setText(_translate("MainWindow", "Show edges img."))
+        self.checkBox_3.setText(_translate("MainWindow", "Show gray img."))
+        self.checkBox_4.setText(_translate("MainWindow", " "))
+        self.checkBox_5.setText(_translate("MainWindow", " "))
+        self.checkBox_6.setText(_translate("MainWindow", " "))
+        self.checkBox_7.setText(_translate("MainWindow", " "))
+        self.checkBox_8.setText(_translate("MainWindow", " "))
+        self.label_tittle.setText(_translate("MainWindow", ".................."))
+        self.menuMain.setTitle(_translate("MainWindow", "Main"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionHelp.setText(_translate("MainWindow", "Help"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.pushButton_1.setText(_translate("MainWindow", "Start"))
+        self.pushButton_2.setText(_translate("MainWindow", "Stop"))
--- src/imageProcesser.py
+++ src/imageProcesser.py
@@ -2,7 +2,7 @@
 import numpy as np
 from collections import defaultdict
 from PyQt5.QtCore import QThread
-import cv2
+import cv2, time
 
 
 class ImageProcesser(QThread):
@@ -13,9 +13,9 @@ class ImageProcesser(QThread):
         self.thresh = 100
         self.minLineSize = 30
 
-        self.visibleFrame = False
-        self.visibleEdges = False
-        self.visibleGray = False
+        self.visibleFrame = True
+        self.visibleEdges = True
+        self.visibleGray = True
 
         self.running = False
 
@@ -24,15 +24,20 @@ class ImageProcesser(QThread):
 
     def run(self):
         self.running = True
-        cap = cv2.VideoCapture(0)
+
+        #cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture('output.avi')
 
         kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-        while (self.running):
-
+        while (self.running and cap.isOpened()):
+            time.sleep(0.03666)
             # Capture frames from the camera
             ret, originalFrame = cap.read()
-
+            if ret == False:
+                print("Erro na leitura da câmera")
+                self.running = False
+                break
             frame = originalFrame[:, 0:300]
 
             ##########################################
@@ -61,7 +66,6 @@ class ImageProcesser(QThread):
                 edges = cv2.bitwise_or(edges, prev2_edges)
             except:
                 edges = edges_simple
-                print("No previous edge")
 
             prev1_edges = edges_simple
             prev2_edges = prev1_edges
@@ -89,7 +93,6 @@ class ImageProcesser(QThread):
 
                 ##########################################
             try:
-
                 lines_p = cv2.HoughLinesP(image=edges, rho=1, theta=np.pi / 100,
                                           threshold=self.thresh, lines=np.array([]),
                                           minLineLength=self.minLineSize, maxLineGap=20)
@@ -114,7 +117,6 @@ class ImageProcesser(QThread):
                     biggestContour = cnt
 
             try:
-                # cv2.drawContours(originalFrame, biggestContour, -1, (0, 255, 0), 3,2)
                 hull = cv2.convexHull(biggestContour, returnPoints=False)
                 defects = cv2.convexityDefects(biggestContour, hull)
                 for i in range(defects.shape[0]):
@@ -130,7 +132,6 @@ class ImageProcesser(QThread):
                         cv2.line(originalFrame, midpoint, far, [0, 255, 125], 2)
                         cv2.circle(originalFrame, far, 5, [0, 0, 255], -1)
 
-
             except:
                 print("No contour found")
                 ##########################################
@@ -140,10 +141,11 @@ class ImageProcesser(QThread):
                 cv2.imshow("Edges", edges)
             if(self.visibleGray==True):
                 cv2.imshow("Gray", gray)
-            # close the output video by pressing 'ESC'
+
             k = cv2.waitKey(5) & 0xFF
-            if k == 27:
+            if k == 27: #esc
                 self.running = False
+                break
 
         cap.release()
         cv2.destroyAllWindows()
@@ -167,15 +169,35 @@ class ImageProcesser(QThread):
 
 
     def dictLines(self,lines):
-        lines_dict = defaultdict(lambda: 0, )
+        lines_dict = {}
         for line in lines:
             rho = line[0][0]
             theta = line[0][1]
             theta_d = int(theta * 180 / np.pi)
-            lines_dict[theta_d] += 1
+            try:
+                lines_dict[theta_d] += 1
+            except:
+                lines_dict[theta_d] = 1
         return lines_dict
 
-
-
+    def cnt_hull_attributes(self,contour):
+        hull = cv2.convexHull(contour)
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        hull_area = cv2.contourArea(hull)
+        solidity = float(area) / hull_area
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = w / h
+        extent = area / (w * h)
+        moment = cv2.moments(contour)
+        attr_dict = {"area": area,
+                     "perimeter": perimeter,
+                     "solitidy": solidity,
+                     "aspect_ratio": aspect_ratio,
+                     "extent": extent}
+        for i in moment.keys():
+            if i[0] == "n":
+                attr_dict[i] = moment[i]
+        return attr_dict
 
 
